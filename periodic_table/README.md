ad5bfea08c2d136585afe23df7a3de609cd28e55
ExplorerJerin <explorer.jerin@gmail.com>
2025-05-01 13:01:02 +0000

    added both the main and also corected the issues that it had while running canada, where the outptu should be none when string cannot be converted

--- periodic_table/README.md
+++ periodic_table/README.md
@@ -5,6 +5,12 @@ https://www.codingame.com/ide/puzzle/periodic-table-spelling
 # To compile the code
 cc  -Wall -Wextra -Werror periodic_table.c 
 
-# To run the code
+# To run the code for main 1
+./a.out
+Hyperpathogenesis
+
+
+# To run the code for main 2
+# this main you can use for specific input elements and it takes the values as command line arguments. 
 ./a.out Hinausgeschossen H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
 
--- periodic_table/periodic_table.c
+++ periodic_table/periodic_table.c
@@ -168,7 +168,9 @@ int pattern_finder(char *word, char **elements, char **output, char **beg)
 	char *split_B;
 	char **split_A;
 	char **start;
-	
+	static int count;
+
+
 	split_A = str_search(word , elements);
 	start = split_A;
 	while(*split_A)
@@ -180,31 +182,74 @@ int pattern_finder(char *word, char **elements, char **output, char **beg)
 			*(output + 1) = NULL;
 			print_Matrix(beg);
 			free(start);
-			return 0;
+			count++;
+			return count;
 		}
 		pattern_finder(split_B,elements,output+1,beg);
 		split_A++;
 	}
         	free(start);
-	return 1;
+	return count;
 }
-
-int main(int argc, char **argv)
+// main 1
+// you can use this in coding game directly
+int main()
 {
-    printf("Number of arguments : %d\n", argc);
-    char *word = *(argv + 1);
-    char **elements = ft_sort(argv + 2);
+    char word[31];
+    scanf("%[^\n]", word);
 
-    int count = str_len(word);
+    char *p = word;
+    char *elements[] = { 
+        "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", 
+        "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", 
+        "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", 
+        "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", 
+        "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", 
+        "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", 
+        "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", 
+        "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", 
+        "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", 
+        "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", 
+        "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", 
+        "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og", NULL
+    };
+    int count = str_len(p);
     char **output = malloc((count + 1) * sizeof(char *));
     char **beg ;
-
+    char **sorted_elements = ft_sort(elements);
     beg = output;  
-    pattern_finder(word, elements, output, beg);
-    if (empty(output) == 0)
+    int pattern_found = pattern_finder(p, sorted_elements, output, beg);
+    if (pattern_found == 0)
 	    printf("none");
-    free(beg);
+    //print_Matrix(output);
+    //free(output);
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
 
- 
+    //printf("spellings\n");
+
     return 0;
 }
+
+// main 2
+// this main you can use for testing and to do specific input of elements and for testing
+
+// int main(int argc, char **argv)
+// {
+//     printf("Number of arguments : %d\n", argc);
+//     char *word = *(argv + 1);
+//     char **elements = ft_sort(argv + 2);
+// 
+//     int count = str_len(word);
+//     char **output = malloc((count + 1) * sizeof(char *));
+//     char **beg ;
+// 
+//     beg = output;  
+//     pattern_finder(word, elements, output, beg);
+//     if (empty(output) == 0)
+// 	    printf("none");
+//     free(beg);
+// 
+//  
+//     return 0;
+// }
